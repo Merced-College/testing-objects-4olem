4641cbe7bd718e4d0817351194f9d081f9aedbc9
4olem <bd360noscope@gmail.com>
2026-07-20 23:55:45 +0000

    new chnages

--- Dog.java
+++ Dog.java
@@ -1,1 +1,37 @@
-//write your code here
+public class Dog {
+    private String name;
+    private int age;
+    private String breed;
+
+    // No-arg constructor
+    public Dog() {
+        name = "Unknown";
+        age = 0;
+        breed = "Mixed";
+    }
+
+    // Parameterized constructor
+    public Dog(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Dog{name='" + name + "', age=" + age + ", breed='" + breed + "'}";
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getBreed() { return breed; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) {
+        if (age >= 0) this.age = age;
+    }
+    public void setBreed(String breed) { this.breed = breed; }
+}
--- ObjectTester.java
+++ ObjectTester.java
@@ -1,1 +1,35 @@
-//write your code here
+public class Album {
+    private string name;
+    private string artist:
+    private double recordsSold;
+
+
+    public Album () {
+        name = "unknown";
+        artist = "unknown";
+        recordsSold = "0.0"; 
+    }
+    //parameterized constructor
+     public Album(String name, String artist, Double recordsSold) {
+        this.name = name;
+        this.artist = artist;
+        this.recordsSold = recordsSold;
+    }
+    // toString
+    @Override
+    public String toString() {
+        return "Album{name='" + name + "', artist=" + artist + ", recordsSold='" + recordsSold + "'}";
+    }
+     // Getters
+    public String getName() { return name; }
+    public int getArtist() { return artist; }
+    public String getRecordsSold() { return recordsSold; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setRecordsSold(Double recordsSold) {
+        if (recordsSold >= 0) this.recordsSold = recordsSold;
+    }
+    public void setArtist(String artist) { this.artist = artist; }
+
+}
